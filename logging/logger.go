d7f6ee55ccf4cd37b2833645cb247f9b32a143df
Chen Shou <cshou@google.com>
2022-05-17 22:07:59 +0000

    fixes

--- logging/logger.go
+++ logging/logger.go
@@ -1,4 +1,4 @@
-// Copyright 2022 the abcxyz authors (see AUTHORS file)
+// Copyright 2022 The Authors (see AUTHORS file)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
