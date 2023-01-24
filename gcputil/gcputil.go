90c4abbcbe25cf54f6ec4bb97f2d971450bf46af
Seth Vargo <seth@sethvargo.com>
2023-01-24 17:51:45 -0600

    Expand logging interceptors, move gcputil to a separate package

---
+++ gcputil/gcputil.go
@@ -0,0 +1,50 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package gpcutil exposes helpers for interacting with Google Cloud.
+package gpcutil
+
+import (
+	"context"
+	"os"
+
+	"cloud.google.com/go/compute/metadata"
+	"github.com/abcxyz/pkg/logging"
+)
+
+// ProjectID attempts to get the project ID, starting with the fastest
+// resolution ($PROJECT_ID, $GOOGLE_PROJECT, $GOOGLE_CLOUD_PROJECT) and then
+// evetually querying the metadata server. If it fails to find a project ID, it
+// logs an error.
+//
+// The results are not cached and could result in outbound HTTP calls. As such,
+// callers should cache the response since it is unlikely to change.
+func ProjectID(ctx context.Context) string {
+	for _, name := range []string{
+		"PROJECT_ID",
+		"GOOGLE_PROJECT",
+		"GOOGLE_CLOUD_PROJECT",
+	} {
+		if v := os.Getenv(name); v != "" {
+			return v
+		}
+	}
+
+	v, err := metadata.ProjectID()
+	if err != nil {
+		logging.FromContext(ctx).Errorw("failed to get project id", "error", err)
+		return ""
+	}
+	return v
+}
