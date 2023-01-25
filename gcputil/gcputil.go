da582a95bb8b29b10d17e5bc0f5bd5033b713754
Seth Vargo <seth@sethvargo.com>
2023-01-24 18:19:32 -0600

    Fix package name

--- gcputil/gcputil.go
+++ gcputil/gcputil.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package gpcutil exposes helpers for interacting with Google Cloud.
-package gpcutil
+// Package gcputil exposes helpers for interacting with Google Cloud.
+package gcputil
 
 import (
 	"context"
