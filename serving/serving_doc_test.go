108b103dd77747052b4c03ce6c55bf01149d6111
Seth Vargo <seth@sethvargo.com>
2023-04-03 19:45:08 -0400

    Add general-purpose serving infrastructure (#103)
    * Add general-purpose serving infrastructure
    
    This standardizes our http/grcp server creation and lifecycle handling.
    
    * Ignore wrapcheck for serving package
    
    * Listen for returned errors too

---
+++ serving/serving_doc_test.go
@@ -0,0 +1,93 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package serving_test
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/abcxyz/pkg/serving"
+	"google.golang.org/grpc"
+)
+
+func Example_hTTP() {
+	// Any cancellable context will work.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+
+	httpServer := &http.Server{
+		ReadHeaderTimeout: 1 * time.Second,
+
+		Handler: mux,
+	}
+
+	server, err := serving.New(os.Getenv("PORT"))
+	if err != nil {
+		panic(err) // TODO: handle error
+	}
+
+	// This will block until the provided context is cancelled.
+	if err := server.StartHTTP(ctx, httpServer); err != nil {
+		panic(err) // TODO: handle error
+	}
+}
+
+func Example_hTTPHandler() {
+	// Any cancellable context will work.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+
+	server, err := serving.New(os.Getenv("PORT"))
+	if err != nil {
+		panic(err) // TODO: handle error
+	}
+
+	// This will block until the provided context is cancelled.
+	if err := server.StartHTTPHandler(ctx, mux); err != nil {
+		panic(err) // TODO: handle error
+	}
+}
+
+func Example_gRPC() {
+	// Any cancellable context will work.
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	grpcServer := grpc.NewServer()
+
+	server, err := serving.New(os.Getenv("PORT"))
+	if err != nil {
+		panic(err) // TODO: handle error
+	}
+
+	// This will block until the provided context is cancelled.
+	if err := server.StartGRPC(ctx, grpcServer); err != nil {
+		panic(err) // TODO: handle error
+	}
+}
