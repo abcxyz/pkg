094e6939fc788aaf410a1791531878ea978fae19
Suhong Qin <51539171+sqin2019@users.noreply.github.com>
2023-10-27 11:23:30 -0700

    feat: add scancode action and reusable workflow (#231)
    Goal: A Github action/workflow to run on workflow_dispatch on the entire
    repo to get all licenses, dependencies, and copyrights in a json and csv
    file.

---
+++ scancode/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -l
+# Copyright 2023 The Authors (see AUTHORS file)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd /scancode-toolkit
+
+./scancode /github/workspace/$1 \
+  --json /github/workspace/scancode.json \
+  --csv /github/workspace/scancode.csv \
+  --license --package --copyright --license-score=70
+
+echo "json=scancode.json" >> $GITHUB_OUTPUT
+echo "csv=scancode.csv" >> $GITHUB_OUTPUT
