b5afa4e0811878da03ef78be5152faf8a2813f99
Ian Shafer <ishafer@google.com>
2025-04-04 22:06:03 +0000

    Fix shell quoting issue
    `shellcheck` requires that variables are double-quoted to avoid globbing
    and word splitting (https://www.shellcheck.net/wiki/SC2086). This means
    that the FILES variable must be an array to have it parsed correctly by
    node.

--- .github/actions/multi-approvers/bin/test.sh
+++ .github/actions/multi-approvers/bin/test.sh
@@ -7,16 +7,15 @@ set -eEuo pipefail
 # directly, because while it has an API, there's no way to force it to transpile
 # the Typescript into JavaScript before passing it to the runner.
 #
-# So we're left with this solution, which shells out to Node to list all files
-# that end in *.test.ts (excluding node_modules/), and then execs out to that
-# process. We have to exec so the stderr/stdout and exit code is appropriately
-# fed to the caller.
+# So we're left with this solution, which finds all non-./node_modules files
+# that end in *.test.ts, and then execs out to that node. We have to exec so the
+# stderr/stdout and exit code is appropriately fed to the caller.
 #
 
-FILES="$(node -e "process.stdout.write(require('node:fs').readdirSync('./', { recursive: true }).filter((e) => {return e.endsWith('.test.ts') && !e.startsWith('node_modules');}).sort().join(' '));")"
+readarray -td '' FILES < <(find * -type f -not -path 'node_modules/*' -name '*.test.ts' -print0 | sort -z)
 
 set -x
 exec node \
   --require ts-node/register \
   --test-reporter spec \
-  --test "${FILES}"
+  --test "${FILES[@]}"
