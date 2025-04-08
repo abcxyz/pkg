639f3ead6e818ff2e87c90392d33def84cd9538e
Ian Shafer <ishafer@google.com>
2025-04-08 16:58:06 +0000

    Refactor github-main tests to be table-driven

--- .github/actions/multi-approvers/tests/github-main.test.ts
+++ .github/actions/multi-approvers/tests/github-main.test.ts
@@ -16,10 +16,23 @@ import assert from "node:assert/strict";
 import { test } from "node:test";
 import * as ghCore from "@actions/core";
 import { context as ghContext } from "@actions/github";
+import { DeepPartial } from "./deep-partial";
 import { main } from "../src/github-main";
 
 type Core = typeof ghCore;
 type Context = typeof ghContext;
+type PartialContext = DeepPartial<Context>;
+
+interface TestCase {
+  description: string;
+  inputs: {
+    [key: string]: string;
+  };
+  contextOverrides: PartialContext;
+  expected: {
+    failMessage: string;
+  };
+}
 
 function newFakeCore(inputs: { [key: string]: string }): Core {
   return {
@@ -30,143 +43,85 @@ function newFakeCore(inputs: { [key: string]: string }): Core {
   } as unknown as Core;
 }
 
-test("#github-main", { concurrency: true }, async (suite) => {
-  await suite.test("should fail on unsupported event", async (t) => {
-    const inputs = {
+const BASE_CONTEXT = {
+  eventName: "pull_request",
+  runId: 1,
+  payload: {
+    pull_request: {
+      number: 1,
+      head: {
+        ref: "fake-branch",
+      },
+    },
+    repository: {
+      name: "fake-repository",
+      owner: {
+        login: "test-org",
+      },
+    },
+  },
+} as PartialContext;
+
+const TEST_CASES = [
+  {
+    description: "should fail on unsupported event",
+    inputs: {
       team: "fake-team",
       token: "fake-token",
-    };
-    const core = newFakeCore(inputs);
-    const setFailed = t.mock.method(core, "setFailed", () => {});
-    const context = {
+    },
+    contextOverrides: {
       eventName: "push",
-      runId: 1,
-      payload: {
-        pull_request: {
-          number: 1,
-          head: {
-            ref: "fake-branch",
-          },
-        },
-        repository: {
-          name: "fake-repository",
-          owner: {
-            login: "test-org",
-          },
-        },
-      },
-    } as unknown as Context;
-
-    await main(core, context);
-
-    assert.equal(setFailed.mock.calls.length, 1);
-    const failMsg = setFailed.mock.calls[0].arguments[0];
-    assert.equal(
-      failMsg,
-      "Multi-approvers action failed: Unexpected event [push].",
-    );
-  });
-
-  await suite.test("fails when no inputs are set", async (t) => {
-    const inputs = {};
-    const core = newFakeCore(inputs);
-    const setFailed = t.mock.method(core, "setFailed", () => {});
-    const context = {
-      eventName: "pull_request",
-      runId: 1,
-      payload: {
-        pull_request: {
-          number: 1,
-          head: {
-            ref: "fake-branch",
-          },
-        },
-        repository: {
-          name: "fake-repository",
-          owner: {
-            login: "test-org",
-          },
-        },
-      },
-    } as unknown as Context;
-
-    await main(core, context);
-
-    assert.equal(setFailed.mock.calls.length, 1);
-    const failMsg = setFailed.mock.calls[0].arguments[0];
-    assert.equal(
-      failMsg,
-      "Multi-approvers action failed: Invalid input(s): token is required; team is required",
-    );
-  });
-
-  await suite.test("fails when token input is not set", async (t) => {
-    const inputs = {
+    },
+    expected: {
+      failMessage: "Multi-approvers action failed: Unexpected event [push].",
+    },
+  },
+  {
+    description: "fails when no inputs are set",
+    inputs: {},
+    expected: {
+      failMessage:
+        "Multi-approvers action failed: Invalid input(s): token is required; team is required",
+    },
+  },
+  {
+    description: "fails when token input is not set",
+    inputs: {
       team: "fake-team",
-    };
-    const core = newFakeCore(inputs);
-    const setFailed = t.mock.method(core, "setFailed", () => {});
-    const context = {
-      eventName: "pull_request",
-      runId: 1,
-      payload: {
-        pull_request: {
-          number: 1,
-          head: {
-            ref: "fake-branch",
-          },
-        },
-        repository: {
-          name: "fake-repository",
-          owner: {
-            login: "test-org",
-          },
-        },
-      },
-    } as unknown as Context;
-
-    await main(core, context);
-
-    assert.equal(setFailed.mock.calls.length, 1);
-    const failMsg = setFailed.mock.calls[0].arguments[0];
-    assert.equal(
-      failMsg,
-      "Multi-approvers action failed: Invalid input(s): token is required",
-    );
-  });
-
-  await suite.test("fails when team input is not set", async (t) => {
-    const inputs = {
+    },
+    expected: {
+      failMessage:
+        "Multi-approvers action failed: Invalid input(s): token is required",
+    },
+  },
+  {
+    description: "fails when team input is not set",
+    inputs: {
       token: "fake-token",
-    };
-    const core = newFakeCore(inputs);
-    const setFailed = t.mock.method(core, "setFailed", () => {});
-    const context = {
-      eventName: "pull_request",
-      runId: 1,
-      payload: {
-        pull_request: {
-          number: 1,
-          head: {
-            ref: "fake-branch",
-          },
-        },
-        repository: {
-          name: "fake-repository",
-          owner: {
-            login: "test-org",
-          },
-        },
-      },
-    } as unknown as Context;
+    },
+    expected: {
+      failMessage:
+        "Multi-approvers action failed: Invalid input(s): team is required",
+    },
+  },
+] as Array<TestCase>;
 
-    await main(core, context);
+test("#github-main", { concurrency: true }, async (suite) => {
+  for (const c of TEST_CASES) {
+    await suite.test(c.description, async (t) => {
+      const core = newFakeCore(c.inputs);
+      const setFailed = t.mock.method(core, "setFailed", () => {});
+      const context = Object.assign(
+        {},
+        BASE_CONTEXT,
+        c.contextOverrides,
+      ) as unknown as Context;
 
-    assert.equal(setFailed.mock.calls.length, 1);
-    const failMsg = setFailed.mock.calls[0].arguments[0];
-    assert.equal(
-      failMsg,
-      "Multi-approvers action failed: Invalid input(s): team is required",
-    );
-  });
+      await main(core, context);
+
+      assert.equal(setFailed.mock.calls.length, 1);
+      const failMsg = setFailed.mock.calls[0].arguments[0];
+      assert.equal(failMsg, c.expected.failMessage);
+    });
+  }
 });
