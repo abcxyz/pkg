2dc5e04ebd948abf55894a7be5dfcf6f82a32d75
pdewilde <pdewilde@google.com>
2025-04-21 15:18:32 -0700

    WIP:
    Simple() tested, looking for overall feedback before committing more thorough testing.
    
    Signed-off-by: pdewilde <pdewilde@google.com>

--- run/exec.go
+++ run/exec.go
@@ -1,10 +1,10 @@
-// Copyright 2025 The Authors (see AUTHORS file)
+// Copyright 2023 The Authors (see AUTHORS file)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,77 +12,331 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package run provides enhanced functionality for executing external commands,
+// offering control over environment variables, process attributes, I/O,
+// timeouts, and integrated logging, using a functional options pattern.
 package run
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/abcxyz/pkg/logging" // Import the specific logging package
 )
 
-// ExecConfig are the inputs for a run operation.
-type Option struct {
-	stdin      io.Writer
-	stdout     io.Writer
-	stderr     io.Writer
-	workingDir string
+// DefaultRunTimeout is how long commands wait if the context doesn't have a deadline.
+const DefaultRunTimeout = time.Minute
 
-	// allowedEnvKeys and deniedEnvKeys respectively define an allow/deny list of
-	// patterns for environment variable keys. Keys are matched using
-	// [filepath.Match].
-	allowedEnvKeys []string
-	deniedEnvKeys  []string
+// DefaultWaitDelay is how long after context cancellation processes are actually killed
+// if another value isn't set. See exec.Cmd.WaitDelay for more information.
+const DefaultWaitDelay = time.Second
 
-	// overrideEnvVars are the environment variables to inject into the child
-	// process, no matter what the user configured. These take precedence over all
-	// other configurables.
-	overrideEnvVars []string
+// Simple is a wrapper around [Run] that captures stdout and stderr as strings.
+// This is intended to be used for commands that run non-interactively then
+// exit.
+//
+// If the command exits with a nonzero status code, an *exec.ExitError will be
+// returned.
+//
+// If the command fails, the error message will include the contents of stdout
+// and stderr. This saves boilerplate in the caller.
+func Simple(ctx context.Context, args ...string) (stdout, stderr string, exitCode int, _ error) {
+	var stdoutBuf, stderrBuf bytes.Buffer
+	opts := []*Option{
+		WithStdout(&stdoutBuf),
+		WithStderr(&stderrBuf),
+	}
+
+	exitCode, err := Run(ctx, opts, args...)
+
+	return stdoutBuf.String(), stderrBuf.String(), exitCode, err
 }
 
-// Run runs the command provided by args, using the options in opts. Error
-// is only returned if something external to the process fails (timeout,
-// command not found, ect). It is up to user to check status code.
+// Run executes the command specified by args, applying configurations from opts.
 //
-// If the incoming context doesn't already have a timeout, then a default
-// timeout will be added (see DefaultRunTimeout).
+// By default, a non-zero exit code results in an error (*exec.ExitError),
+// unless the AllowNonzeroExit option is used.
+// The error message includes stdout/stderr content if they are captured to buffers.
 //
-// opts may be nil if no special options are needed.
+// If the context doesn't have a deadline, DefaultRunTimeout is applied.
 //
-// This doesn't execute a shell (unless of course command is the name of a shell
+// The input args must have len>=1. opts may be nil if no special options are
+// needed.
+//
+// This doesn't execute a shell (unless of course args[0] is the name of a shell
 // binary).
+func Run(ctx context.Context, opts []*Option, args ...string) (exitCode int, _ error) {
+	logger := logging.FromContext(ctx)
 
-// TODO: what are the ergonomics like with opts not being varadic operator? I'm not sure I'm happy with this
-func Run(ctx context.Context, opts []*Option, command string, args ...string) (exitCode int, _ error) {
+	if len(args) == 0 {
+		logger.DebugContext(ctx, "run called with no arguments")
+		return -1, errors.New("run: must provide at least one argument (the command)")
+	}
+
+	// Apply default timeout if none exists on the context.
 	if _, ok := ctx.Deadline(); !ok {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, DefaultRunTimeout)
-		defer cancel()
+		defer cancel() // Ensure the derived context is cancelled
 	}
 
-	cmd := exec.CommandContext(ctx, command, args...) //nolint:gosec // run'ing the input args is fundamentally the whole point
-	setSysProcAttr(cmd)
-	setCancel(cmd)
-	// any of these can be nil
 	compiledOpts := compileOpts(opts)
-	cmd.Stdout = compiledOpts.stdout
-	cmd.Stderr = compiledOpts.stderr
-	cmd.Stdin = compiledOpts.stdin
-	cmd.Dir = compiledOpts.cwd
 
-	err := cmd.Run()
+	// #nosec G204 -- Execution of external commands is the purpose of this package. Inputs must be trusted by the caller.
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+
+	cmd.Dir = compiledOpts.cwd
+	cmd.Stdin = compiledOpts.stdin
+	// Handle stdout/stderr, capturing output if writers are bytes.Buffer for error reporting.
+	var stdoutBuf, stderrBuf *bytes.Buffer
+	if compiledOpts.stdout != nil {
+		cmd.Stdout = compiledOpts.stdout
+		if bb, ok := compiledOpts.stdout.(*bytes.Buffer); ok {
+			stdoutBuf = bb
+		}
+	} else {
+		cmd.Stdout = os.Stdout
+	}
+	if compiledOpts.stderr != nil {
+		cmd.Stderr = compiledOpts.stderr
+		if bb, ok := compiledOpts.stderr.(*bytes.Buffer); ok {
+			stderrBuf = bb
+		}
+	} else {
+		cmd.Stderr = os.Stderr
+	}
+
+	useCustomEnv := len(compiledOpts.allowedEnvKeys) > 0 || len(compiledOpts.deniedEnvKeys) > 0 || len(compiledOpts.additionalEnv) > 0
+	if useCustomEnv {
+		currentEnv := os.Environ()
+		logger.DebugContext(ctx, "calculating custom environment",
+			"inherited_count", len(currentEnv),
+			"allowed_patterns", compiledOpts.allowedEnvKeys,
+			"denied_patterns", compiledOpts.deniedEnvKeys,
+			"additional_vars_count", len(compiledOpts.additionalEnv))
+		env := environ(currentEnv, compiledOpts.allowedEnvKeys, compiledOpts.deniedEnvKeys, compiledOpts.additionalEnv)
+		logger.DebugContext(ctx, "computed environment", "env", env)
+		cmd.Env = env
+	} else {
+		logger.DebugContext(ctx, "using inherited environment")
+	}
+
+	if compiledOpts.waitDelay != nil {
+		cmd.WaitDelay = *compiledOpts.waitDelay
+	} else {
+		cmd.WaitDelay = DefaultWaitDelay
+	}
+	logger.DebugContext(ctx, "set wait delay", "delay", cmd.WaitDelay)
+
+	logger.DebugContext(ctx, "starting command", "args", args, "options", compiledOpts)
+	err := cmd.Run() // This blocks until the command exits or context is cancelled
+
+	// Exit code -1 if ProcessState is nil (e.g., start failed)
+	exitCode = -1
+	if cmd.ProcessState != nil {
+		exitCode = cmd.ProcessState.ExitCode()
+	}
+
 	if err != nil {
-		// Don't return error if both (a) the caller indicated they're OK with a
-		// nonzero exit code and (b) the error is of a type that means the only
-		// problem was a nonzero exit code.
 		var exitErr *exec.ExitError
-		if errors.As(err, &exitErr) && compiledOpts.allowNonZeroExit {
+		isExitError := errors.As(err, &exitErr)
+
+		if isExitError && compiledOpts.allowNonZeroExit {
+			logger.DebugContext(ctx, "command exited non-zero, but allowed by option", "exit_code", exitCode)
 			err = nil
+		} else if isExitError {
+			stdoutContent := "[stdout not captured]"
+			if stdoutBuf != nil {
+				stdoutContent = stdoutBuf.String()
+			}
+			stderrContent := "[stderr not captured]"
+			if stderrBuf != nil {
+				stderrContent = stderrBuf.String()
+			}
+			err = fmt.Errorf("command %v exited non-zero (%d): %w (context error: %v)\nstdout:\n%s\nstderr:\n%s",
+				args, exitCode, err, ctx.Err(), stdoutContent, stderrContent)
+			logger.DebugContext(ctx, "command exited non-zero", "exit_code", exitCode, "error", err)
 		} else {
-			err = fmt.Errorf(`run of %v failed: error was "%w", context error was "%w"\nstdout: %s\nstderr: %s`,
-				args, err, ctx.Err(), cmd.Stdout, cmd.Stderr)
+			// It's an actual execution error (e.g., command not found, context cancelled early)
+			stdoutContent := "[stdout not captured]"
+			if stdoutBuf != nil {
+				stdoutContent = stdoutBuf.String()
+			}
+			stderrContent := "[stderr not captured]"
+			if stderrBuf != nil {
+				stderrContent = stderrBuf.String()
+			}
+			err = fmt.Errorf("command %v failed: %w (context error: %v)\nstdout:\n%s\nstderr:\n%s",
+				args, err, ctx.Err(), stdoutContent, stderrContent)
+			logger.DebugContext(ctx, "command failed with execution error", "exit_code", exitCode, "error", err)
+		}
+	} else {
+		// Command ran successfully (exit code 0)
+		logger.DebugContext(ctx, "command finished successfully", "exit_code", exitCode)
+	}
+
+	return exitCode, err
+}
+
+// Option implements the functional options pattern for [Run].
+// It holds all configurable settings for executing a command.
+type Option struct {
+	// Basic execution control
+	allowNonZeroExit bool           // If true, non-zero exit codes are not treated as errors.
+	cwd              string         // Working directory for the command.
+	stdin            io.Reader      // Standard input for the command.
+	stdout           io.Writer      // Standard output destination.
+	stderr           io.Writer      // Standard error destination.
+	waitDelay        *time.Duration // Grace period after context cancel before killing.
+
+	// Environment control
+	allowedEnvKeys []string // Glob patterns for inherited env vars to allow.
+	deniedEnvKeys  []string // Glob patterns for inherited env vars to deny.
+	additionalEnv  []string // Env vars ("KEY=VALUE") to add/override.
+}
+
+// AllowNonzeroExit prevents Run from returning an error
+// when the command exits with a non-zero status code.
+func AllowNonzeroExit() *Option {
+	return &Option{allowNonZeroExit: true}
+}
+
+// WithStdin provides the given reader as the command's
+// standard input.
+func WithStdin(stdin io.Reader) *Option {
+	return &Option{stdin: stdin}
+}
+
+// WithStdinStr is a convenience option that uses the given string as the
+// command's standard input.
+func WithStdinStr(stdin string) *Option {
+	return WithStdin(bytes.NewBufferString(stdin))
+}
+
+// WithStdout directs the command's standard output
+// to the given writer.
+func WithStdout(stdout io.Writer) *Option {
+	return &Option{stdout: stdout}
+}
+
+// WithStderr directs the command's standard error
+// to the given writer.
+func WithStderr(stderr io.Writer) *Option {
+	return &Option{stderr: stderr}
+}
+
+// WithCwd runs the command in the specified working directory.
+func WithCwd(cwd string) *Option {
+	return &Option{cwd: cwd}
+}
+
+// WithFilteredEnv returns an option that filters the inherited environment variables.
+// allowed is a list of glob patterns for keys to keep (empty means allow all initially).
+// denied is a list of glob patterns for keys to remove (takes precedence).
+func WithFilteredEnv(allowed, denied []string) *Option {
+	return &Option{allowedEnvKeys: allowed, deniedEnvKeys: denied}
+}
+
+// WithAdditionalEnv returns an option that adds or overrides environment variables.
+// vars is a slice of strings in "KEY=VALUE" format.can
+func WithAdditionalEnv(vars []string) *Option {
+	return &Option{additionalEnv: vars}
+}
+
+// WithWaitDelay returns an option that sets a grace period for the command to
+// exit after the context is cancelled before being forcefully killed.
+// Default: DefautWaitDelay.
+// See exec.Cmd.WaitDelay for more information.
+func WithWaitDelay(d time.Duration) *Option {
+	return &Option{waitDelay: &d}
+}
+
+// TODO: handle process group and cancel semantics.
+// WithProcessGroup returns an option that attempts to run the command in a new
+// process group (primarily effective on Unix-like systems).
+//func WithProcessGroup(set bool) *Option {
+//	return &Option{setProcessGroup: set}
+//}
+
+// compileOpts merges multiple options into a single configuration struct.
+// The last option specified for most fields takes precedence.
+// additionalEnv is appended across options.
+func compileOpts(opts []*Option) *Option {
+	out := Option{}
+
+	for _, opt := range opts {
+		if opt.allowNonZeroExit {
+			out.allowNonZeroExit = true
+		}
+		if opt.cwd != "" {
+			out.cwd = opt.cwd
+		}
+		if opt.stdin != nil {
+			out.stdin = opt.stdin
+		}
+		if opt.stdout != nil {
+			out.stdout = opt.stdout
+		}
+		if opt.stderr != nil {
+			out.stderr = opt.stderr
+		}
+		if opt.waitDelay != nil {
+			out.waitDelay = opt.waitDelay
+		}
+		if opt.allowedEnvKeys != nil {
+			out.allowedEnvKeys = opt.allowedEnvKeys
+		}
+		if opt.deniedEnvKeys != nil {
+			out.deniedEnvKeys = opt.deniedEnvKeys
+		}
+		if len(opt.additionalEnv) > 0 {
+			out.additionalEnv = append(out.additionalEnv, opt.additionalEnv...)
 		}
 	}
-	return cmd.ProcessState.ExitCode(), err
+	return &out
+}
+
+// anyGlobMatch checks if string s matches any of the glob patterns.
+func anyGlobMatch(s string, patterns []string) bool {
+	for _, p := range patterns {
+		if p == "*" || p == ".*" { // Common fast-path wildcards
+			return true
+		}
+	}
+	for _, p := range patterns {
+		if ok, _ := filepath.Match(p, s); ok { // Ignore Match errors
+			return true
+		}
+	}
+	return false
+}
+
+// environ compiles the appropriate environment to pass to the child process.
+// The overridden environment is always added, even if not explicitly
+// allowed/denied.
+func environ(osEnv, allowedKeys, deniedKeys, overrideEnv []string) []string {
+	finalEnv := make([]string, 0, len(osEnv)+len(overrideEnv))
+
+	// Select keys that match the allow filter (if given) but not the deny filter.
+	for _, v := range osEnv {
+		k := strings.SplitN(v, "=", 2)[0]
+		if (len(allowedKeys) == 0 || anyGlobMatch(k, allowedKeys)) &&
+			!anyGlobMatch(k, deniedKeys) {
+			finalEnv = append(finalEnv, v)
+		}
+	}
+
+	// Add overrides at the end, after any filtering. os/exec has "last wins"
+	// semantics, so appending is an overriding action.
+	finalEnv = append(finalEnv, overrideEnv...)
+
+	return finalEnv
 }
---
+++ run/exec_test.go
@@ -0,0 +1,61 @@
+package run
+
+import (
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+// this may not be portable
+func TestSimple(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name       string
+		args       []string
+		wantOut    string
+		wantStdErr string
+		wantCode   int
+		wantErr    string
+	}{
+		{
+			name:    "happy_path",
+			args:    []string{"echo", "foo", "bar   bazz"},
+			wantOut: "foo bar   bazz\n",
+		},
+		{
+			name:     "no_command_error",
+			args:     []string{"echoooocrapimistyped", "foo", "bar   bazz"},
+			wantCode: -1,
+			wantErr:  "not found",
+		},
+		{
+			name:       "exit_code_error",
+			args:       []string{"cat", "/fake/file/path/should/fail"},
+			wantCode:   1,
+			wantStdErr: "cat: /fake/file/path/should/fail: No such file or directory\n",
+			wantErr:    "exited non-zero (1): exit status 1 (context error: <nil>)\nstdout:\n\nstderr:\ncat:",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := t.Context()
+			stdout, stderr, exitCode, err := Simple(ctx, tc.args...)
+
+			if diff := cmp.Diff(stdout, tc.wantOut); diff != "" {
+				t.Errorf("stdout was not as expected(-got,+want): %s", diff)
+			}
+			if diff := cmp.Diff(stderr, tc.wantStdErr); diff != "" {
+				t.Errorf("stderr was not as expected(-got,+want): %s", diff)
+			}
+			if got, want := exitCode, tc.wantCode; got != want {
+				t.Errorf("exit code was not as expected, got: %d, want: %d", got, want)
+			}
+			if diff := testutil.DiffErrString(err, tc.wantErr); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
